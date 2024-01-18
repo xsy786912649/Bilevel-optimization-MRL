452a7117a0fed3dd5e8f0020eb641e80fbe98a5a
xsy786912649 <786912649@qq.com>
2024-01-18 09:41:44 -0500

    update

--- BOMRL_discrete/graph.py
+++ BOMRL_discrete/graph.py
@@ -16,7 +16,7 @@ from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 from cycler import cycler
 matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['ps.fonttype'] = 42
-default_cycler = (cycler(color=['#295778', '#ee7663', '#62c5cc', '#f3b554', '#cc87f8']) )  
+default_cycler = (cycler(color=['#295778', '#62c5cc', '#ee7663', '#f3b554', '#577829']) )  
 plt.rc('axes', prop_cycle=default_cycler)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -90,7 +90,7 @@ plt.plot(axis,data_hole_1_optimal,'--', linewidth=2.5 ,label="Optimal task-speci
 #plt.xticks(np.arange(0,iterations,40))
 plt.title('High task variance ($\mathcal{A l g}^{(1)}$ applied)',size=28)
 plt.xlabel('Number of policy adaptation steps',size=28)
-plt.ylabel("Accumulated reward",size=28)
+plt.ylabel("Expected Accumulated reward",size=28)
 plt.ylim(-0.5,1.75)
 #plt.legend(loc=4)
 plt.legend(loc=0, numpoints=1)
@@ -116,7 +116,7 @@ plt.plot(axis,data_no_1_optimal,'--', linewidth=2.5 ,label="Optimal task-specifi
 #plt.xticks(np.arange(0,iterations,40))
 plt.title('Low task variance ($\mathcal{A l g}^{(1)}$ applied)',size=28)
 plt.xlabel('Number of policy adaptation steps',size=28)
-plt.ylabel("Accumulated reward",size=28)
+plt.ylabel("Expected Accumulated reward",size=28)
 plt.ylim(-0.25,1.75)
 #plt.legend(loc=4)
 plt.legend(loc=0, numpoints=1)
@@ -142,11 +142,11 @@ plt.plot(axis,data_hole_2_optimal,'--' , linewidth=2.5 ,label="Optimal task-spec
 #plt.xticks(np.arange(0,iterations,40))
 plt.title('High task variance ($\mathcal{A l g}^{(2)}$ applied)',size=28)
 plt.xlabel('Number of policy adaptation steps',size=28)
-plt.ylabel("Accumulated reward",size=28)
+plt.ylabel("Expected Accumulated reward",size=28)
 plt.ylim(-0.3,1.75)
 #plt.legend(loc=4)
 plt.legend(loc=0, numpoints=1)
-plt.subplots_adjust(left=0.129, right=0.993, top=0.923, bottom=0.126)
+plt.subplots_adjust(left=0.113, right=0.993, top=0.923, bottom=0.126)
 leg = plt.gca().get_legend()
 ltext = leg.get_texts()
 #plt.setp(ltext, fontsize=18,fontweight='bold') 
@@ -168,7 +168,7 @@ plt.plot(axis,data_no_2_optimal,'--', linewidth=2.5 ,label="Optimal task-specifi
 #plt.xticks(np.arange(0,iterations,40))
 plt.title('Low task variance ($\mathcal{A l g}^{(2)}$ applied)',size=28)
 plt.xlabel('Number of policy adaptation steps',size=28)
-plt.ylabel("Accumulated reward",size=28)
+plt.ylabel("Expected Accumulated reward",size=28)
 plt.ylim(-0.25,1.75)
 #plt.legend(loc=4)
 plt.legend(loc=0, numpoints=1)
@@ -187,84 +187,92 @@ line_width = 1
 
 fig, ax = plt.subplots(figsize=(8*1.1,6*1.1))
 
-y_data = [ data_hole_1_mean[0], data_hole_1_mean[1], 1.30, 1.6381797250192158]
-x_data = ('No\n adaptation', 'One-step\n of $\mathcal{A l g}^{(1)}$', 'One-step of\n policy gradient', 'Optimal\n policy')
-std_err=[data_hole_1_sd[0]/2,data_hole_1_sd[1]/2,data_hole_1_sd[1]/2,0] 
+y_optimal=1.6381797250192158
+y_data = y_optimal - np.array([ data_hole_1_mean[0], data_hole_1_mean[1], 1.30])
+#y_data = [ data_hole_1_mean[0], data_hole_1_mean[1], 1.30, 1.6381797250192158]
+x_data = ('No\n adaptation', 'One-step\n of $\mathcal{A l g}^{(1)}$', 'One-step of\n policy gradient')
+std_err=[data_hole_1_sd[0]/2,data_hole_1_sd[1]/2,data_hole_1_sd[1]/2] 
 
 error_params=dict(elinewidth=4,capsize=5)
 
-bar = plt.bar(x_data, y_data, width=bar_width, linewidth=line_width ,yerr=std_err,error_kw=error_params, color=['#295778', '#ee7663', '#62c5cc', '#f3b554', '#cc87f8'], edgecolor='black')
+bar = plt.bar(x_data, y_data, width=bar_width, linewidth=line_width ,yerr=std_err,error_kw=error_params, color=['#577829', '#295778', '#ee7663' ], edgecolor='black')
 
-plt.text(-0.28, 1.6335099673006876-0.2774698 * 9 * 0.1 - 0.25, 'Lower bound of\n one-step $\mathcal{A l g}^{(1)}$', fontsize=20)
-plt.axhline(y=1.6335099673006876-0.2774698 * 9 * 0.1 , color='black', linestyle='--', linewidth=4, zorder=3)
+plt.text(1-0.35, 0.2674698 * 9 * 0.1 + 0.05, 'Upper bound for\n one-step $\mathcal{A l g}^{(1)}$', fontsize=20)
+plt.axhline(y=0.2474698 * 9 * 0.1 , color='black', linestyle='--', linewidth=4, zorder=3)
 
 plt.title('High task variance ($\mathcal{A l g}^{(1)}$ applied)',size=28)
-ax.set_ylabel("Accumulated reward",size=28)
-plt.subplots_adjust(left=0.136, right=0.989, top=0.923, bottom=0.131)
+ax.set_ylabel("Expected Optimality Gap",size=28)
+plt.subplots_adjust(left=0.115, right=0.983, top=0.923, bottom=0.131)
 plt.savefig('./figures/hole_1_bound.pdf') 
 plt.show()
 
 
 fig, ax = plt.subplots(figsize=(8*1.1,6*1.1))
 
-y_data = [ data_hole_2_mean[0], data_hole_2_mean[1], 1.30, 1.6381797250192158]
-x_data = ('No\n adaptation', 'One-step\n of $\mathcal{A l g}^{(2)}$', 'One-step of\n policy gradient', 'Optimal\n policy')
-std_err=[data_hole_2_sd[0]/2,data_hole_2_sd[1]/2,data_hole_2_sd[1]/2,0] 
+y_optimal=1.6381797250192158
+y_data = y_optimal - np.array([ data_hole_2_mean[0], data_hole_2_mean[1], 1.30])
+#y_data = [ data_hole_2_mean[0], data_hole_2_mean[1], 1.30, 1.6381797250192158]
+x_data = ('No\n adaptation', 'One-step\n of $\mathcal{A l g}^{(2)}$', 'One-step of\n policy gradient')
+std_err=[data_hole_2_sd[0]/2,data_hole_2_sd[1]/2,data_hole_2_sd[1]/2] 
 
 error_params=dict(elinewidth=4,capsize=5)
 
-bar = plt.bar(x_data, y_data, width=bar_width, linewidth=line_width ,yerr=std_err,error_kw=error_params, color=['#295778', '#ee7663', '#62c5cc', '#f3b554', '#cc87f8'], edgecolor='black')
+bar = plt.bar(x_data, y_data, width=bar_width, linewidth=line_width ,yerr=std_err,error_kw=error_params, color=['#577829', '#295778', '#ee7663' ], edgecolor='black')
 
-plt.text(-0.28, 1.6381797250192158-0.6193449 * 9 * 0.1 - 0.25, 'Lower bound of\n one-step $\mathcal{A l g}^{(2)}$', fontsize=20)
-plt.axhline(y=1.6381797250192158-0.6193449 * 9 * 0.1 , color='black', linestyle='--', linewidth=4, zorder=3)
+plt.text(1-0.35, 0.6193449 * 9 * 0.1 - 0.2, 'Upper bound for\n one-step $\mathcal{A l g}^{(2)}$', fontsize=20)
+plt.axhline(y=0.6193449 * 9 * 0.1 , color='black', linestyle='--', linewidth=4, zorder=3)
 
 plt.title('High task variance ($\mathcal{A l g}^{(2)}$ applied)',size=28)
-ax.set_ylabel("Accumulated reward",size=28)
-plt.subplots_adjust(left=0.136, right=0.989, top=0.923, bottom=0.131)
+ax.set_ylabel("Expected Optimality Gap",size=28)
+plt.subplots_adjust(left=0.136, right=0.983, top=0.923, bottom=0.131)
 plt.savefig('./figures/hole_2_bound.pdf') 
 plt.show()
 
 
 
 fig, ax = plt.subplots(figsize=(8*1.1,6*1.1))
+y_optimal=1.6238083970192134
 
-y_data = [ data_no_1_mean[0], data_no_1_mean[1], 1.5031502591632093, 1.6238083970192134]
-x_data = ('No\n adaptation', 'One-step\n of $\mathcal{A l g}^{(1)}$', 'One-step of\n policy gradient', 'Optimal\n policy')
-std_err=[data_no_1_sd[0]/2,data_no_1_sd[1]/2,data_no_1_sd[1]/2,0] 
+y_data = y_optimal - np.array([ data_no_1_mean[0], data_no_1_mean[1], 1.5031502591632093 ])
+#y_data = [ data_no_1_mean[0], data_no_1_mean[1], 1.5031502591632093, 1.6238083970192134]
+x_data = ('No\n adaptation', 'One-step\n of $\mathcal{A l g}^{(1)}$', 'One-step of\n policy gradient')
+std_err=[data_no_1_sd[0]/2,data_no_1_sd[1]/2,data_no_1_sd[1]/2] 
 
 error_params=dict(elinewidth=4,capsize=5)
 
-bar = plt.bar(x_data, y_data, width=bar_width, linewidth=line_width ,yerr=std_err,error_kw=error_params, color=['#295778', '#ee7663', '#62c5cc', '#f3b554', '#cc87f8'], edgecolor='black')
+bar = plt.bar(x_data, y_data, width=bar_width, linewidth=line_width ,yerr=std_err,error_kw=error_params, color=['#577829', '#295778', '#ee7663' ], edgecolor='black')
 
-plt.text(-0.28, 1.6238083970192134-0.15265574 * 9* 0.1 +0.012  , 'Lower bound of\n one-step $\mathcal{A l g}^{(1)}$', fontsize=20)
-plt.axhline(y=1.6238083970192134-0.15265574 * 9* 0.1 , color='black', linestyle='--', linewidth=4, zorder=3)
-plt.ylim(1.35,1.64)
+plt.text(1-0.35, 0.15265574 * 9* 0.1 +0.01  , 'Upper bound for\n one-step $\mathcal{A l g}^{(1)}$', fontsize=20)
+plt.axhline(y=0.15265574 * 9* 0.1 , color='black', linestyle='--', linewidth=4, zorder=3)
+plt.ylim(0.0,0.199)
 
 plt.title('Low task variance ($\mathcal{A l g}^{(1)}$ applied)',size=28)
-ax.set_ylabel("Accumulated reward",size=28)
-plt.subplots_adjust(left=0.136, right=0.989, top=0.923, bottom=0.131)
+ax.set_ylabel("Expected Optimality Gap",size=28)
+plt.subplots_adjust(left=0.136, right=0.983, top=0.923, bottom=0.131)
 plt.savefig('./figures/nohole_1_bound.pdf') 
 plt.show()
 
 
 fig, ax = plt.subplots(figsize=(8*1.1,6*1.1))
 
-y_data = [ data_no_2_mean[0], data_no_2_mean[1], 1.5031502591632093, 1.6142083970192134]
-x_data = ('No\n adaptation', 'One-step\n of $\mathcal{A l g}^{(2)}$', 'One-step of\n policy gradient', 'Optimal\n policy')
-std_err=[data_no_2_sd[0]/2,data_no_2_sd[1]/2,data_no_2_sd[1]/2,0] 
+y_optimal=1.6142083970192134
+y_data = y_optimal - np.array([ data_no_2_mean[0], data_no_2_mean[1], 1.5031502591632093 ])
+#y_data = [ data_no_2_mean[0], data_no_2_mean[1], 1.5031502591632093, 1.6142083970192134]
+x_data = ('No\n adaptation', 'One-step\n of $\mathcal{A l g}^{(2)}$', 'One-step of\n policy gradient')
+std_err=[data_no_2_sd[0]/2,data_no_2_sd[1]/2,data_no_2_sd[1]/2] 
 
 error_params=dict(elinewidth=4,capsize=5)
 
-bar = plt.bar(x_data, y_data, width=bar_width, linewidth=line_width ,yerr=std_err,error_kw=error_params, color=['#295778', '#ee7663', '#62c5cc', '#f3b554', '#cc87f8'], edgecolor='black')
+bar = plt.bar(x_data, y_data, width=bar_width, linewidth=line_width ,yerr=std_err,error_kw=error_params, color=['#577829', '#295778', '#ee7663' ], edgecolor='black')
 
 
-plt.text(0.32,  1.6142083970192134 - 0.35228756 * 9.0* 0.05 - 0.04  , 'Lower bound of\n one-step $\mathcal{A l g}^{(2)}$', fontsize=20)
-plt.axhline( y= 1.6142083970192134 - 0.35228756 * 9.0* 0.05, color='black', linestyle='--', linewidth=4, zorder=3)
-plt.ylim(1.35,1.64)
+plt.text(1-0.35,  0.35228756 * 9.0* 0.05 - 0.025  , 'Upper bound for\n one-step $\mathcal{A l g}^{(2)}$', fontsize=20)
+plt.axhline( y= 0.35228756 * 9.0* 0.05, color='black', linestyle='--', linewidth=4, zorder=3)
+plt.ylim(0.0,0.18)
 
 plt.title('Low task variance ($\mathcal{A l g}^{(2)}$ applied)',size=28)
-ax.set_ylabel("Accumulated reward",size=28)
-plt.subplots_adjust(left=0.136, right=0.989, top=0.923, bottom=0.131)
+ax.set_ylabel("Expected Optimality Gap",size=28)
+plt.subplots_adjust(left=0.136, right=0.983, top=0.923, bottom=0.131)
 plt.savefig('./figures/nohole_2_bound.pdf') 
 plt.show() 
 
@@ -272,42 +280,47 @@ plt.show()
 '''--------------------------------------------------'''
 fig, ax = plt.subplots(figsize=(8*1.1,6*1.1))
 
-y_data = [0.41384084713453, 1.31245, 1.3049245332733332, 1.6335099673006876]
-x_data = ('No\n adaptation', 'One-step\n of $\mathcal{A l g}^{(3)}$', 'One-step of\n policy gradient', 'Optimal\n policy')
-std_err=[data_hole_2_sd[0]/2,data_hole_2_sd[1]/2,data_hole_2_sd[1]/2,0] 
+y_optimal=1.6335099673006876
+y_data = y_optimal - np.array([ 0.41384084713453, 1.31245, 1.3049245332733332 ])
+#y_data = [0.41384084713453, 1.31245, 1.3049245332733332, 1.6335099673006876]
+x_data = ('No\n adaptation', 'One-step\n of $\mathcal{A l g}^{(3)}$', 'One-step of\n policy gradient')
+std_err=[data_hole_2_sd[0]/2,data_hole_2_sd[1]/2,data_hole_2_sd[1]/2] 
 
 error_params=dict(elinewidth=4,capsize=5)
 
-bar = plt.bar(x_data, y_data, width=bar_width, linewidth=line_width ,yerr=std_err,error_kw=error_params, color=['#295778', '#ee7663', '#62c5cc', '#f3b554', '#cc87f8'], edgecolor='black')
+bar = plt.bar(x_data, y_data, width=bar_width, linewidth=line_width ,yerr=std_err,error_kw=error_params, color=['#577829', '#295778', '#ee7663' ], edgecolor='black')
 
-plt.text(0.32, 0.1933 + 0.05, 'Lower bound of\n one-step $\mathcal{A l g}^{(3)}$', fontsize=20)
-plt.axhline(y=0.1933 , color='black', linestyle='--', linewidth=4, zorder=3)
+plt.text(1-0.35, y_optimal-0.1933 - 0.28, 'Upper bound for\n one-step $\mathcal{A l g}^{(3)}$', fontsize=20)
+plt.axhline(y=y_optimal-0.1933 , color='black', linestyle='--', linewidth=4, zorder=3)
+plt.ylim(0.0,1.8)
 
 plt.title('High task variance ($\mathcal{A l g}^{(3)}$ applied)',size=28)
-ax.set_ylabel("Accumulated reward",size=28)
-plt.subplots_adjust(left=0.136, right=0.989, top=0.923, bottom=0.131)
+ax.set_ylabel("Expected Optimality Gap",size=28)
+plt.subplots_adjust(left=0.136, right=0.983, top=0.923, bottom=0.131)
 plt.savefig('./figures/hole_3_bound.pdf') 
 plt.show()
 
 
 fig, ax = plt.subplots(figsize=(8*1.1,6*1.1))
 
-y_data = [ 1.491573045339209, 1.51521, 1.5031502591632093, 1.6142083970192134]
-x_data = ('No\n adaptation', 'One-step\n of $\mathcal{A l g}^{(3)}$', 'One-step of\n policy gradient', 'Optimal\n policy')
-std_err=[data_no_2_sd[0]/2,data_no_2_sd[1]/2,data_no_2_sd[1]/2,0] 
+y_optimal=1.6142083970192134
+y_data = y_optimal - np.array([ 1.491573045339209, 1.51521, 1.5031502591632093 ])
+#y_data = [ 1.491573045339209, 1.51521, 1.5031502591632093, 1.6142083970192134]
+x_data = ('No\n adaptation', 'One-step\n of $\mathcal{A l g}^{(3)}$', 'One-step of\n policy gradient')
+std_err=[data_no_2_sd[0]/2,data_no_2_sd[1]/2,data_no_2_sd[1]/2] 
 
 error_params=dict(elinewidth=4,capsize=5)
 
-bar = plt.bar(x_data, y_data, width=bar_width, linewidth=line_width ,yerr=std_err,error_kw=error_params, color=['#295778', '#ee7663', '#62c5cc', '#f3b554', '#cc87f8'], edgecolor='black')
+bar = plt.bar(x_data, y_data, width=bar_width, linewidth=line_width ,yerr=std_err,error_kw=error_params, color=['#577829', '#295778', '#ee7663' ], edgecolor='black')
 
 
-plt.text(0.32,  0.9132  + 0.04  , 'Lower bound of\n one-step $\mathcal{A l g}^{(3)}$', fontsize=20)
-plt.axhline( y= 0.9132 , color='black', linestyle='--', linewidth=4, zorder=3)
-plt.ylim(0.8,1.64)
+plt.text(1-0.35,  y_optimal-0.9132  - 0.12  , 'Upper bound for\n one-step $\mathcal{A l g}^{(3)}$', fontsize=20)
+plt.axhline( y= y_optimal-0.9132 , color='black', linestyle='--', linewidth=4, zorder=3)
+plt.ylim(0.0,0.799)
 
 plt.title('Low task variance ($\mathcal{A l g}^{(3)}$ applied)',size=28)
-ax.set_ylabel("Accumulated reward",size=28)
-plt.subplots_adjust(left=0.136, right=0.989, top=0.923, bottom=0.131)
+ax.set_ylabel("Expected Optimality Gap",size=28)
+plt.subplots_adjust(left=0.115, right=0.983, top=0.923, bottom=0.131)
 plt.savefig('./figures/nohole_3_bound.pdf') 
 plt.show() 
 
